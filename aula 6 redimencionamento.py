f22c352c76fccf1f44a48c602c00300304023f28
jolobos <digo8432@gmail.com>
2022-10-23 23:46:43 -0300

    aula 6 scrollview

---
+++ aula 6 redimencionamento.py
@@ -0,0 +1,19 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.scrollview import ScrollView
+
+
+class Tarefas(ScrollView):
+    def __init__(self, tarefas, **kwargs):
+        super().__init__(**kwargs)
+        for tarefa in tarefas:
+            self.ids.box.add_widget(Label(text=tarefa, font_size=30, size_hint_y=None, height=200))
+
+
+class Test6(App):
+    def build(self):
+        return Tarefas(['fazer', 'buscar', 'o lokoh!!!', 'kdksadjsadk', 'sdnsadj', 'asndsahas'])
+
+
+Test6().run()
